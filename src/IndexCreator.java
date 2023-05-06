c20ea071076c6ae3c33ae4e542e0d082df9b4eca
g-poulos <cs04480@uoi.gr>
2023-05-06 18:24:06 +0300

    Creating Index

--- src/IndexCreator.java
+++ src/IndexCreator.java
@@ -1,14 +1,24 @@
 package src;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.lang.*;
 
 public class IndexCreator {
 
-    public static ArrayList<String[]> readData(String filename) {
+    public ArrayList<String[]> readData(String filename) {
         String line;
         String artist;
         String song;
@@ -44,14 +54,44 @@ public class IndexCreator {
 
     }
 
-    public static void main(String[] args){
-        ArrayList<String[]> textList = readData("spotify_millsongdata_cl.csv");
-        for (String[] i: textList) {
-            System.out.println(i[0]);
-            System.out.println(i[1]);
-            System.out.println(i[2]);
-            System.out.println("--------------------");
+
+    public Directory createIndex(ArrayList<String[]> songs, StandardAnalyzer analyzer) throws IOException {
+
+        Path path = Paths.get(System.getProperty("user.dir") + "/index");
+        System.out.println();
+        FSDirectory index = FSDirectory.open(path);
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter w = new IndexWriter(index, config);
+
+        for (String[] line: songs) {
+            addDoc(w, line);
         }
+
+        w.close();
+        return index;
+    }
+
+    private static void addDoc(IndexWriter w, String[] line) throws IOException {
+        Document doc = new Document();
+        doc.add(new TextField("artist", line[0], Field.Store.YES));         // TODO: Check the difference StringField/TextField
+        doc.add(new TextField("song", line[1], Field.Store.YES));
+        doc.add(new TextField("text", line[2], Field.Store.YES));
+        w.addDocument(doc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        IndexCreator idxCreator = new IndexCreator();
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+
+        ArrayList<String[]> textList = idxCreator.readData("spotify_millsongdata_cl.csv");
+        idxCreator.createIndex(textList, analyzer);
+
+//        for (String[] i: textList) {
+//            System.out.println(i[0]);
+//            System.out.println(i[1]);
+//            System.out.println(i[2]);
+//            System.out.println("--------------------");
+//        }
     }
 
 }

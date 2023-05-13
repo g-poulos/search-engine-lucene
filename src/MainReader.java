85fa606917c575a346f08d1cd4422c4b781d58f7
g-poulos <cs04480@uoi.gr>
2023-05-13 13:31:11 +0300

    MainReader before changes

--- src/MainReader.java
+++ src/MainReader.java
@@ -1,14 +1,17 @@
 package src;
 
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.*;
 import org.apache.lucene.store.FSDirectory;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -21,11 +24,14 @@ import java.util.List;
 public class MainReader {
     private ArrayList<Document> foundDocuments;
     private List<List<Document>> documentPages = new ArrayList<>();
+    private ArrayList<StringBuilder> htmlDocuments;
+    private List<List<StringBuilder>> htmlPages = new ArrayList<>();
 
-
-    public void runQuery(String queryStr, String field) throws IOException, ParseException {
+    public void runQuery(String queryStr, String field) throws IOException, ParseException, InvalidTokenOffsetsException {
         foundDocuments = new ArrayList<>();
         documentPages = new ArrayList<>();
+        htmlDocuments = new ArrayList<>();
+        htmlPages = new ArrayList<>();
 
         Path path = Paths.get(System.getProperty("user.dir") + "/index");
         FSDirectory index = FSDirectory.open(path);
@@ -34,12 +40,12 @@ public class MainReader {
         Query query = new QueryParser(field, analyzer).parse(queryStr);
 
         int hitsPerPage = 100;
-        org.apache.lucene.index.IndexReader reader = DirectoryReader.open(index);
+        IndexReader reader = DirectoryReader.open(index);
         IndexSearcher searcher = new IndexSearcher(reader);
         TopDocs docs = searcher.search(query, hitsPerPage);
         ScoreDoc[] hits = docs.scoreDocs;
 
-        fillFoundDocuments(searcher, hits);
+        fillFoundDocuments(searcher, hits, query);
         System.out.println("Found " + hits.length + " hits.");
 
         createPages();
@@ -61,12 +67,54 @@ public class MainReader {
         documentPages.add(page);
     }
 
-    private void fillFoundDocuments(IndexSearcher searcher, ScoreDoc[] hits) throws IOException {
+    private void fillFoundDocuments(IndexSearcher searcher, ScoreDoc[] hits, Query q) throws IOException, InvalidTokenOffsetsException {
+        SimpleHTMLFormatter formatter = new SimpleHTMLFormatter();
+        QueryScorer scorer = new QueryScorer(q, "song");
+        Highlighter highlighter = new Highlighter(formatter, scorer);
+        highlighter.setTextFragmenter(new SimpleFragmenter(50));
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+
+
+        StringBuilder resultBuilder;
+        htmlDocuments = new ArrayList<>();
+
         for(int i = 0; i< hits.length; ++i) {
             int docId = hits[i].doc;
             Document d = searcher.doc(docId);
             foundDocuments.add(d);
+
+            resultBuilder = new StringBuilder();
+            String text = d.get("song");
+            TokenStream tokenStream = TokenSources.getTokenStream("song", text, analyzer);
+            String highlightedText = highlighter.getBestFragment(tokenStream, text);
+
+
+            resultBuilder.append(d.get("artist") + " - ");
+            resultBuilder.append(highlightedText);
+//            resultBuilder.append( "<br>" + d.get("text").substring(0, 200) + "<br>");
+            htmlDocuments.add(resultBuilder);
+            System.out.println(resultBuilder);
         }
+
+        ArrayList<StringBuilder> page = new ArrayList<>();
+        int batch = 0;
+        for (StringBuilder doc: htmlDocuments) {
+            if (batch == 10) {
+                htmlPages.add(page);
+                page = new ArrayList<>(Arrays.asList(doc));
+                batch = 1;
+            }else {
+                page.add(doc);
+                batch = batch + 1;
+            }
+        }
+        htmlPages.add(page);
+
+
+    }
+
+    private void highlightQueryResult() {
+
     }
 
     public ArrayList<Document> getFoundDocuments() {
@@ -76,5 +124,14 @@ public class MainReader {
     public List<List<Document>> getDocumentPages() {
         return documentPages;
     }
+
+    public ArrayList<StringBuilder> getHtmlDocuments() {
+        return htmlDocuments;
+    }
+
+    public List<List<StringBuilder>> getHtmlPages() {
+        return htmlPages;
+    }
+
 }
 
